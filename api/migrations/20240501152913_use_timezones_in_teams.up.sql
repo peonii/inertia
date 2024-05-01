95d94a442de291c1212c12eaae9b08a5fdda2575
peony <mail@peony.wtf>
2024-05-01 17:34:41 +0200

    safsdf

--- api/migrations/20240501152913_use_timezones_in_teams.up.sql
+++ api/migrations/20240501152913_use_timezones_in_teams.up.sql
@@ -1,1 +1,1 @@
-alter table teams alter column veto_period_end t type timestamptz not null using veto_period_end at time zone 'UTC';
+alter table teams alter column veto_period_end type timestamptz not null using veto_period_end at time zone 'UTC';
