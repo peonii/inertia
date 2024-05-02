f81e1bb75167383442a63678cc0c0d93cb0052ae
peony <54771118+peonii@users.noreply.github.com>
2024-05-02 12:48:49 +0200

    new socket types

--- api/internal/api/ws.go
+++ api/internal/api/ws.go
@@ -20,13 +20,15 @@ type wsClient struct {
 type wsHub struct {
 	Clients      map[*wsClient]bool
 	BroadcastLoc chan wsLocationMsg
+	BroadcastPwp chan wsPowerupMsg
+	BroadcastCat chan wsCatchMsg
 	Register     chan *wsClient
 	Unregister   chan *wsClient
 
 	logger      *zap.Logger
 	powerupRepo repository.PowerupRepository
 	teamRepo    repository.TeamRepository
-  userRepo    repository.UserRepository
+	userRepo    repository.UserRepository
 }
 
 type wsMsg struct {
@@ -41,9 +43,26 @@ type wsLocationMsg struct {
 }
 
 type wsLocationPayload struct {
-  Location domain.LocationCreate `json:"loc"`
-  Team     *domain.Team           `json:"team"`
-  User     *domain.User           `json:"user"`
+	Location domain.LocationCreate `json:"loc"`
+	Team     *domain.Team          `json:"team"`
+	User     *domain.User          `json:"user"`
+}
+
+type wsPowerupMsg struct {
+	Powerup domain.Powerup `json:"pwp"`
+}
+
+type wsPowerupPayload struct {
+	Powerup domain.Powerup `json:"pwp"`
+	Caster  *domain.Team   `json:"cas"`
+}
+
+type wsCatchMsg struct {
+	NewRunnerID string `json:"nrid"`
+}
+
+type wsCatchPayload struct {
+	NewRunner *domain.Team `json:"nrt"`
 }
 
 func NewWsHub(logger *zap.Logger, db *pgxpool.Pool) *wsHub {
@@ -56,7 +75,7 @@ func NewWsHub(logger *zap.Logger, db *pgxpool.Pool) *wsHub {
 		logger:      logger,
 		powerupRepo: repository.MakePostgresPowerupRepository(db),
 		teamRepo:    repository.MakePostgresTeamRepository(db),
-    userRepo:    repository.MakePostgresUserRepository(db),
+		userRepo:    repository.MakePostgresUserRepository(db),
 	}
 }
 
@@ -76,17 +95,17 @@ func (h *wsHub) Run() {
 			h.logger.Info("broadcasting location", zap.Any("message", message))
 			var powerupsByGameID map[string][]*domain.Powerup
 
-      sender, err := h.userRepo.FindOne(context.Background(), message.UserID)
-      if err != nil {
-        h.logger.Error("failed to find user", zap.Error(err))
-        return
-      }
+			sender, err := h.userRepo.FindOne(context.Background(), message.UserID)
+			if err != nil {
+				h.logger.Error("failed to find user", zap.Error(err))
+				return
+			}
 
-      senderTeam, err := h.teamRepo.FindByGameUser(context.Background(), message.GameID, message.UserID)
-      if err != nil {
-        h.logger.Error("failed to find team", zap.Error(err))
-        return
-      }
+			senderTeam, err := h.teamRepo.FindByGameUser(context.Background(), message.GameID, message.UserID)
+			if err != nil {
+				h.logger.Error("failed to find team", zap.Error(err))
+				return
+			}
 
 			for client := range h.Clients {
 				h.logger.Info("sending to client", zap.Any("client", client))
@@ -113,41 +132,98 @@ func (h *wsHub) Run() {
 					continue
 				}
 
-        override := false
-        neverShow := false
+				override := false
+				neverShow := false
 
 				for _, powerup := range powerups {
 					if powerup.CasterID == team.ID {
-            if powerup.Type == domain.PowerupTypeRevealHunters {
-              override = true
-            }
+						if powerup.Type == domain.PowerupTypeRevealHunters {
+							override = true
+						}
 					} else if powerup.CasterID == senderTeam.ID {
-            if powerup.Type == domain.PowerupTypeHideTracker {
-              neverShow = true
-            }
-          }
+						if powerup.Type == domain.PowerupTypeHideTracker {
+							neverShow = true
+						}
+					}
 				}
 
-        if neverShow {
-          continue
-        }
+				if neverShow {
+					continue
+				}
 
 				if client.isRunner && !override {
 					continue // only hunters/spectators should receive location updates
 				}
 
-        payload := wsLocationPayload{
-          Location: message.Location,
-          User: sender,
-          Team: senderTeam,
-        }
+				payload := wsLocationPayload{
+					Location: message.Location,
+					User:     sender,
+					Team:     senderTeam,
+				}
 
-        h.logger.Info("sending payload",
-          zap.Any("payload", payload),
-        )
-  
+				h.logger.Info("sending payload",
+					zap.Any("payload", payload),
+				)
+
 				client.conn.Send(wsMsg{
 					Type: "loc",
+					Data: payload,
+				})
+			}
+		case message := <-h.BroadcastPwp:
+			h.logger.Info("broadcasting powerup", zap.Any("message", message))
+
+			caster, err := h.teamRepo.FindOne(context.Background(), message.Powerup.CasterID)
+			if err != nil {
+				h.logger.Error("failed to find team", zap.Error(err))
+				return
+			}
+
+			for client := range h.Clients {
+				h.logger.Info("sending to client", zap.Any("client", client))
+				if client.gameID != caster.GameID {
+					continue
+				}
+
+				payload := wsPowerupPayload{
+					Powerup: message.Powerup,
+					Caster:  caster,
+				}
+
+				h.logger.Info("sending payload",
+					zap.Any("payload", payload),
+				)
+
+				client.conn.Send(wsMsg{
+					Type: "pwp",
+					Data: payload,
+				})
+			}
+		case message := <-h.BroadcastCat:
+			h.logger.Info("broadcasting catch", zap.Any("message", message))
+
+			newRunner, err := h.teamRepo.FindOne(context.Background(), message.NewRunnerID)
+			if err != nil {
+				h.logger.Error("failed to find team", zap.Error(err))
+				return
+			}
+
+			for client := range h.Clients {
+				h.logger.Info("sending to client", zap.Any("client", client))
+				if client.gameID != newRunner.GameID {
+					continue
+				}
+
+				payload := wsCatchPayload{
+					NewRunner: newRunner,
+				}
+
+				h.logger.Info("sending payload",
+					zap.Any("payload", payload),
+				)
+
+				client.conn.Send(wsMsg{
+					Type: "cat",
 					Data: payload,
 				})
 			}
